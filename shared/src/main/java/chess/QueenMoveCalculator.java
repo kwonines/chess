ae40e997d25f23f074c64db1a3170d1301f205f3
kwonines <lion_onines@yahoo.com>
2025-05-08 18:21:29 -0600

    Simplify QueenMoveCalculator to simply add together the results of a Rook's available moves and a Bishop's available moves.

--- shared/src/main/java/chess/QueenMoveCalculator.java
+++ shared/src/main/java/chess/QueenMoveCalculator.java
@@ -5,75 +5,12 @@ import java.util.ArrayList;
 public class QueenMoveCalculator extends MoveCalculator {
     @Override
     public ArrayList<ChessMove> availableMoves(ChessBoard board, ChessPiece self, ChessPosition position) {
-        ArrayList<ChessMove> moveList = new ArrayList<>();
 
-        for (int row = position.getRow() + 1, col = position.getColumn() + 1; row < 9 && col < 9; row++, col++) {
-            if (insertMoveThenBreak(board, self, position, moveList, row, col)) {
-                break;
-            }
-        }
-
-        for (int row = position.getRow() - 1, col = position.getColumn() + 1; row > 0 && col < 9; row--, col++) {
-            if (insertMoveThenBreak(board, self, position, moveList, row, col)) {
-                break;
-            }
-        }
-
-        for (int row = position.getRow() - 1, col = position.getColumn() - 1; row > 0 && col > 0; row--, col--) {
-            if (insertMoveThenBreak(board, self, position, moveList, row, col)) {
-                break;
-            }
-        }
-        for (int row = position.getRow() + 1, col = position.getColumn() - 1; row < 9 && col > 0; row++, col--) {
-            if (insertMoveThenBreak(board, self, position, moveList, row, col)) {
-                break;
-            }
-        }
-
-        for (int row = position.getRow() + 1; row < 9; row++) {
-            if (insertMoveThenBreak(board, self, position, moveList, row, position.getColumn())) {
-                break;
-            }
-        }
-
-        for (int col = position.getColumn() + 1; col < 9; col++) {
-            if (insertMoveThenBreak(board, self, position, moveList, position.getRow(), col)) {
-                break;
-            }
-        }
-
-        for (int row = position.getRow() - 1; row > 0; row--) {
-            if (insertMoveThenBreak(board, self, position, moveList, row, position.getColumn())) {
-                break;
-            }
-        }
-
-        for (int col = position.getColumn() - 1; col > 0; col--) {
-            if (insertMoveThenBreak(board, self, position, moveList, position.getRow(), col)) {
-                break;
-            }
-        }
-
-
+        BishopMoveCalculator bishopMoveCalculator = new BishopMoveCalculator();
+        ArrayList<ChessMove> moveList = bishopMoveCalculator.availableMoves(board, self, position);
+        RookMoveCalculator rookMoveCalculator = new RookMoveCalculator();
+        moveList.addAll(rookMoveCalculator.availableMoves(board, self, position));
 
         return moveList;
-    }
-
-    //given a row and column to move to, will insert that as a move if the space is empty or occupied as an enemy.
-    //If the space is an enemy, it will also return true. If the space is an ally, it will just return true.
-    private boolean insertMoveThenBreak(ChessBoard board, ChessPiece self, ChessPosition position, ArrayList<ChessMove> moveList, int row, int col) {
-        ChessPosition moveSpace;
-        SpaceType moveSpaceType;
-        moveSpace = new ChessPosition(row, col);
-        moveSpaceType = findSpaceType(board, self, moveSpace);
-        if (moveSpaceType == SpaceType.EMPTY) {
-            moveList.add(new ChessMove(position, moveSpace));
-        } else if (moveSpaceType == SpaceType.ENEMY) {
-            moveList.add(new ChessMove(position, moveSpace));
-            return true;
-        } else {
-            return true;
-        }
-        return false;
     }
 }
