b312a2369f3922c847a23331a7d6afa86f2ad4ea
kwonines <lion_onines@yahoo.com>
2025-05-10 11:05:06 -0600

    Generate equals and hashCode overrides for ChessGame. Pass all tests

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a gameBoard
@@ -15,6 +16,20 @@ public class ChessGame {
 
     public ChessGame() {
         gameBoard.resetBoard();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return turn == chessGame.turn && Objects.equals(gameBoard, chessGame.gameBoard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(turn, gameBoard);
     }
 
     /**
